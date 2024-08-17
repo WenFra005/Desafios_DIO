2582012aab14e6eb905048f329f876fd6382e0ea
WenFra005 <wendellfrancisco2005@hotmail.com>
2024-08-17 17:26:58 -0300

    Adição do README

---
+++ API-Spring/README.md
@@ -0,0 +1,8 @@
+# API hospitalar
+Código feito para o cumprimento do desafio DIO de fazer um API próprio. Esse código faz o cadastro de usuário que podem ser **pacientes** ou **médicos**, agenda consultas, mostra notícias do hospital e gerencia dados informações pessoais hospitalares e profissionais.
+
+## ATENÇÃO
+Os dados contidos no código são fictícios. Usei o **Faker** para isso.
+
+## Diagrama de classes
+
--- API-Spring/desafio/src/main/java/desafio/dio/hospital/Application.java
+++ API-Spring/desafio/src/main/java/desafio/dio/hospital/Application.java
@@ -1,13 +1,13 @@
-package desafio.dio.desafio;
+package desafio.dio.hospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
